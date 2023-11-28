f6f6157fb58fac42d3b03086b9ce856b01e8199c
Zephic06 <70858757+Zephic06@users.noreply.github.com>
2023-11-28 09:19:52 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,9 @@
+/* */
 
+
+CREATE TABLE producten (id INTEGER PRIMARY KEY, naam TEXT, aantal INTEGER, kleur TEXT, materiaal TEXT,  prijs INTEGER); /*Tabel toevoegen */
+
+INSERT INTO producten (1, "Jordan 4", 1, "Pure Money", "Leer", 350); /*product toevoegen */
+INSERT INTO producten (2, "Jordan 4", 1, "Black Cats", "Leer", 650); /*product toevoegen */
+INSERT INTO producten (3, "Jordan 4", 1, "Oreo", "Leer", 450); /*product toevoegen */
+INSERT INTO prodcuten (4, "Jordan 4", 1, "Red Thunder", "Suède", 500); /*product toevoegen */
