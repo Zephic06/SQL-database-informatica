8788f3cb861c6dc93bbc96440bb9c7505d3efc7a
Zephic06 <70858757+Zephic06@users.noreply.github.com>
2023-11-28 09:28:15 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -7,3 +7,8 @@ INSERT INTO producten (1, "Jordan 4", 1, "Pure Money", "Leer", 350); /*product t
 INSERT INTO producten (2, "Jordan 4", 1, "Black Cats", "Leer", 650); /*product toevoegen */
 INSERT INTO producten (3, "Jordan 4", 1, "Oreo", "Leer", 450); /*product toevoegen */
 INSERT INTO prodcuten (4, "Jordan 4", 1, "Red Thunder", "Suède", 500); /*product toevoegen */
+INSERT INTO producten (5, "Jordan 4", 1, "Yellow Thunder", "Suède", 500); /*product toevoegen */
+
+SELECT * FROM producten ORDER BY prijs;
+SELECT naam, materiaal FROM producten ORDER BY id;
+SELECT * FROM producten WHERE prijs < 500 ORDER BY prijs;
